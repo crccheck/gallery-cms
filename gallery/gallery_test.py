519b79eece576eb4bd70e93c52285bd8ae566d52
crccheck <c@crccheck.com>
2016-09-03 05:13:09 +0000

    fix how test changed the fixture

--- gallery/gallery_test.py
+++ gallery/gallery_test.py
@@ -1,32 +1,51 @@
 import os
+import random
+import shutil
 from unittest.mock import patch
 
+import pytest
 from aiohttp.test_utils import make_mocked_request
 from multidict import MultiDict
 
 from .gallery import save, settings, Item
 
 
-BASE_DIR = os.path.abspath(os.path.join('..', os.path.dirname(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FIXTURES_DIR = os.path.join(BASE_DIR, 'fixtures/')
 pytest_plugins = 'aiohttp.pytest_plugin'
 
 
-async def test_handler_save(test_client):
+@pytest.fixture
+def jpeg(request):
+    """Make a temp JPEG. Returns the path relative to FIXTURES_DIR."""
+    def fin():
+        os.unlink(os.path.join(FIXTURES_DIR, 'tmpLenna.jpg'))
+
+    shutil.copyfile(os.path.join(FIXTURES_DIR, 'Lenna.jpg'),
+                    os.path.join(FIXTURES_DIR, 'tmpLenna.jpg'))
+    request.addfinalizer(fin)
+    return 'tmpLenna.jpg'
+
+
+async def test_handler_save(jpeg):
+    headline = 'headline {}'.format(random.randint(0, 99))
+    caption = 'caption {}'.format(random.randint(0, 99))
+
     async def post():
         return MultiDict({
-            'src': '/Lenna.jpg',
-            'Iptc.Application2.Headline': 'headline',
-            'Iptc.Application2.Caption': 'caption',
-            'Iptc.Application2.Keywords': ''
+            'src': jpeg,
+            'Iptc.Application2.Headline': headline,
+            'Iptc.Application2.Caption': caption,
         })
 
     req = make_mocked_request('post', '/foo/')
     req.post = post
 
-    with patch.object(settings, 'STORAGE_DIR', new=os.path.join(BASE_DIR, 'fixtures')):
+    with patch.object(settings, 'STORAGE_DIR', new=FIXTURES_DIR):
         resp = await save(req)
 
         assert resp.status == 200
 
-        item = Item('/Lenna.jpg')
-        assert item.meta['Iptc.Application2.Headline'].value == ['headline']
+        item = Item(jpeg)
+        assert item.meta['Iptc.Application2.Headline'].value == [headline]
+        assert item.meta['Iptc.Application2.Caption'].value == [caption]
