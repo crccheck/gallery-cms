a0ac6d30dbd67f2a0bab35ce57d80df22ea90c69
crccheck <c@crccheck.com>
2016-09-06 03:40:03 +0000

    Submit form over ajax

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,6 @@ import 'lazysizes'
 
 // FIXME this causes the browser to freeze because there are so many elements
 $('form.thumb').one('click', (evt) => {
-  console.log(evt)
   const $elem = $(evt.currentTarget)
   $elem.find('select').select2({
     tags: true
@@ -13,4 +12,22 @@ $('form.thumb').one('click', (evt) => {
   if (evt.target.tagName === 'SELECT') {
     $elem.find('input.select2-search__field').focus()
   }
+})
+
+$('form').on('submit', function (evt) {
+  evt.preventDefault()
+
+  const $form = $(this)
+  console.log($form.attr('action'), $form.serialize())
+  $.post($form.attr('action'), $form.serialize())
+  .done(function (data) {
+    const $button = $form.find('button')
+    $button.text('saved!').addClass('success')
+    setTimeout(() => {
+      $button.text('save').removeClass('success')
+    }, 2000)
+  })
+  .fail(function (xhr, textStatus, message) {
+    window.alert(message)
+  })
 })
